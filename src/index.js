7cc0867a691a4efb51f23fe4b60cc3eb1e15a79b
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2023-07-05 22:16:38 +0530

    Added Middleware

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
+// function created using Curried concept - logger(obj, next, action) 
+// logger(obj)(next)(action)
+// const logger = function ({ dispatch, getState }) {
+//   return function (next) {
+//     return function (action) {       // Method 1 middleware 
+//       //  middleware code
+//       console.log('ACTION_TYPE = ', action.type);
+//       next(action);
+//     }
+//   }
+// }
 
+// Method 2 to write middleware
+
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  // logger code
+  console.log('ACTION_TYPE = ',action.type);
+  next(action);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = configureStore({reducer: rootReducer});
+const store = configureStore({reducer: rootReducer}, applyMiddleware(logger));
 console.log('store', store);
 // console.log('Before State', store.getState());
 
